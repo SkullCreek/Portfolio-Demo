f1670d83dea6d5479960c01243ef176d7b2f9fcc
SkullCreek <darpan.bahadur.2001@gmail.com>
2021-11-06 10:16:23 +0530

    added text

--- js/script.js
+++ js/script.js
@@ -26,8 +26,22 @@ $(function(){
 
 //slicknav coding
 
+
+
 // start big slider
 $(document).ready(function() {
-    $(".skitter-large").skitter();
+    $(".skitter-large").skitter({
+        label : false,
+        dots :false,
+    });
   });
+
+  $(document).ready(function(){
+    
+    $(".picone-header").slideDown(1000,function(){
+        $(".picone-btn").fadeIn(1500,function(){
+            $(".picone-header, .picone-btn").delay(100).fadeOut();
+        });
+    });
+});
 //end big slider
